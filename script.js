b7397f088d6a09f9f5db08d4b4cc621e0a2a6145
Daniel Hickey <daniellhickey@gmail.com>
2016-04-28 16:53:47 +0100

    Doing a small test with a nested for loop to print a grid of circles. I used two ramdom colour generator from my colour selector project in my JavascriptProjects repository.

---
+++ script.js
@@ -0,0 +1,7 @@
+var myCircle = new Path.Circle(new Point(100, 70), 50);
+myCircle.fillColor = 'white';
+
+var myCircle2 = new Path.Circle(new Point(50, 50), 300);
+myCircle2.fillColor = 'pink';
+
+alert("working");

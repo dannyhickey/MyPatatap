5e93bf7d17d23a1e083222d8f022a1ebf9ab5c9d
Daniel Hickey <daniellhickey@gmail.com>
2016-04-29 23:16:14 +0100

    Minor changes

--- script.js
+++
@@ -1,7 +0,0 @@
-var myCircle = new Path.Circle(new Point(100, 70), 50);
-myCircle.fillColor = 'white';
-
-var myCircle2 = new Path.Circle(new Point(50, 50), 300);
-myCircle2.fillColor = 'pink';
-
-alert("working");
